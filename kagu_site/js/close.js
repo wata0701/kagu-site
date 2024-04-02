ed43976281d8adb88d28da3d0374ff1477d681bf
wata0701 <165627237+wata0701@users.noreply.github.com>
2024-04-02 17:49:25 +0900

    Delete kagu_site directory

--- kagu_site/js/close.js
+++
@@ -1,6 +0,0 @@
-const clos = document.querySelector('#close_menu');
-
-clos.addEventListener('mouseup', function (){
-    ham.classList.remove('active'); //ハンバーガーメニューにactiveクラスを外し
-    nav.classList.remove('active'); //ナビゲーションメニューにactiveクラスを外し
-});
